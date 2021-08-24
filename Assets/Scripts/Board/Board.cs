9869febc9624f9ee1fed182364b4a5f74b26854b
ThallesVicenzo <77857665+ThallesVicenzo@users.noreply.github.com>
2021-08-24 15:18:54 -0300

    First Commit

---
+++ Assets/Scripts/Board/Board.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public Dictionary<Vector3Int, TileLogic> tiles;
+
+    public List<Floor> floors;
+
+    public static Board instance;
+
+    [HideInInspector]
+    public Grid grid;
+
+    void Awake()
+    {
+
+        tiles = new Dictionary<Vector3Int, TileLogic>();
+
+        instance = this;
+
+        grid = GetComponent<Grid>();
+    }
+
+    void Start()
+    {
+
+        InitSequence();
+        Debug.Log("foram criados: " + tiles.Count + " tiles");
+
+    }
+
+    public void InitSequence()
+    {
+        LoadFloors();
+    }
+
+    void LoadFloors()
+    {
+
+        for (int i = 0; i < floors.Count; i++)
+        {
+
+            List<Vector3Int> floorTiles = floors[i].LoadTiles();
+
+            for (int j = 0; j < floorTiles.Count; j++)
+            {
+
+                if (!tiles.ContainsKey(floorTiles[j]))
+                {
+
+                    CreateTile(floorTiles[j], floors[i]);
+                }
+            }
+        }
+    }
+
+    void CreateTile(Vector3Int pos, Floor floor)
+    {
+
+        Vector3 worldPos = grid.CellToWorld(pos);
+
+        worldPos.y += floor.tilemap.tileAnchor.y / 2 - 0.5f;
+
+        TileLogic tileLogic = new TileLogic(pos, worldPos, floor);
+
+        tiles.Add(pos, tileLogic);
+    }
+
+
+    void ShadowOrdering()
+    {
+
+        foreach (TileLogic t in tiles.Values)
+        {
+
+            int floorIndex = floors.IndexOf(t.floor);
+            floorIndex -= 2;
+
+            if (floorIndex >= floors.Count && floorIndex < 0)
+            {
+                continue;
+            }
+
+            Floor floorToCheck = floors[floorIndex];
+
+            Vector3Int pos = t.pos;
+
+            IsNECheck(floorToCheck, t, pos + Vector3Int.right);
+
+            IsNECheck(floorToCheck, t, pos + Vector3Int.up);
+
+            IsNECheck(floorToCheck, t, pos + Vector3Int.right + pos + Vector3Int.up);
+        }
+    }
+
+    void IsNECheck(Floor floor, TileLogic t, Vector3Int NEPosition)
+    {
+
+        if (floor.tilemap.HasTile(NEPosition))
+        {
+            t.contentOrder = floor.order;
+        }
+    }
+
+    public static TileLogic GetTile(Vector3Int pos){
+        TileLogic tile = null;
+
+        instance.tiles.TryGetValue(pos, out tile);
+
+        return tile;
+    }
+
+
+
+}
---
+++ Assets/Scripts/Character/Movement.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public bool teste;
+
+    const float MoveSpeed = 0.5f;
+
+    const float jumpHeight = 0.5f;
+
+    public List<Vector3Int> path;
+
+    SpriteRenderer SR;
+
+    Transform jumper;
+
+    TileLogic tileAtual;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        jumper = transform.Find("Jumper");
+        SR = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (teste)
+        {
+            teste = false;
+            StopAllCoroutines();
+            StartCoroutine(Move());
+        }
+    }
+
+    IEnumerator Move()
+    {
+        tileAtual = Board.GetTile(path[0]);
+        transform.position = tileAtual.worldPos;
+
+        for(int i = 1; i<path.Count; i++){
+
+            TileLogic to = Board.GetTile(path[i]);
+
+            if(to == null)
+                continue;
+
+            tileAtual.content = null;
+            
+            if(tileAtual.floor != to.floor){
+                yield return StartCoroutine(Jump(to));
+            }
+            else{
+                yield return StartCoroutine(Walk(to));
+            }
+            
+        }
+    }
+
+    IEnumerator Walk(TileLogic to){
+
+        int id = LeanTween.move(transform.gameObject, to.worldPos, MoveSpeed).id;
+        tileAtual = to;
+
+        yield return new WaitForSeconds(MoveSpeed * 0.5f);
+        SR.sortingOrder = to.contentOrder;
+
+        while(LeanTween.descr(id) != null){
+            yield return null;
+        }
+
+        to.content = this.gameObject;
+
+    }
+
+    IEnumerator Jump(TileLogic to)
+    {
+
+        int id1 = LeanTween.move(transform.gameObject, to.worldPos, MoveSpeed).id;
+        
+        LeanTween.moveLocalY(jumper.gameObject, jumpHeight, MoveSpeed * 0.5f).
+        setLoopPingPong(1).setEase(LeanTweenType.easeInOutQuad);
+
+        float timerOrderUpdate = MoveSpeed;
+
+        if(tileAtual.floor.tilemap.tileAnchor.y > to.floor.tilemap.tileAnchor.y){
+            timerOrderUpdate *= 0.85f;
+        }
+        else{
+            timerOrderUpdate *= 0.2f;
+        }
+        yield return new WaitForSeconds(timerOrderUpdate);
+
+        tileAtual = to;
+
+        SR.sortingOrder = to.contentOrder;
+
+        while(LeanTween.descr(id1) != null){
+            yield return null;
+        }
+
+        to.content = this.gameObject;
+
+    }
+
+}
