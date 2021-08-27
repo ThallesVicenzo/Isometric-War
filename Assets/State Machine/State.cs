19156bd68a0f287bc90f59a81bf68e17d8851477
ThallesVicenzo <77857665+ThallesVicenzo@users.noreply.github.com>
2021-08-27 15:56:23 -0300

    State Machine Added

---
+++ Assets/State Machine/State.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class State : MonoBehaviour
+{
+   public virtual void Enter(){
+
+   }
+
+
+   public virtual void Exit(){
+       
+   }
+}
---
+++ Assets/State Machine/StateMachineController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachineController : MonoBehaviour
+{
+    
+    public static StateMachineController instance;
+    State _current;
+    bool busy;
+    public State current{get{return _current;}}
+
+    void Awake(){
+        instance = this;
+    }
+
+    void Start(){
+        // ChangeTo<AudioDataLoadState>();
+    }
+
+    public void ChangeTO<T>() where T:State{
+
+        State state = GetState<T>();
+
+        if(_current != state)
+            ChangeState(state);
+    }
+
+    public T GetState<T>() where T:State{
+
+        T target = GetComponent<T>();
+
+        if(target != null)
+            target = gameObject.AddComponent<T>();
+
+            return target;
+    }
+
+    protected void ChangeState(State value){
+
+        if(busy)
+            return;
+        busy = true;
+
+        if(_current != null){
+            _current.Exit();
+        }
+
+        _current = value;
+
+        if(_current != null)
+            _current.Enter();
+
+        busy = false;
+    }
+}
