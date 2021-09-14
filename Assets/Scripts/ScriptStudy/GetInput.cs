2b7baca7d1b6ab748bb2d091fa1026b6460e9a5b
ThallesVicenzo <77857665+ThallesVicenzo@users.noreply.github.com>
2021-09-14 15:57:25 -0300

    Testes

---
+++ Assets/Scripts/ScriptStudy/GetInput.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate void Controle(object sender, ref Somatoria somatoria);
+public class GetInput : MonoBehaviour
+{
+
+    Controle sum;
+    Somatoria somatoria;
+    float juros = 0.10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        somatoria = new Somatoria(100000.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.C)){
+            sum = (object sender, ref Somatoria somatoria) => new Somatoria(somatoria.soma *= juros);
+        }
+
+        if(Input.GetKeyDown(KeyCode.P)){
+
+            if(sum != null){
+                sum(null, ref somatoria);
+                Debug.Log(string.Format("Resultado: {0}", somatoria.soma));
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ScriptStudy/Somatoria.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Somatoria
+{
+    public float soma{ get; set;}
+
+    public Somatoria() { }
+
+    public Somatoria(float soma){
+        this.soma = soma;
+    }
+}
